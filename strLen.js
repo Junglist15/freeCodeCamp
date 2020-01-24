32a56042af030074d5419b68af3dbaee897a77c2
root <root@DESKTOP-F4U1U0B.localdomain>
2020-01-24 21:27:08 +0200

    3rd push

---
+++ strLen.js
@@ -0,0 +1,16 @@
+// Example
+var firstNameLength = 0;
+var firstName = "Ada";
+
+firstNameLength = firstName.length;
+
+// Setup
+var lastNameLength = 0;
+var lastName = "Lovelace";
+
+// Only change code below this line.
+
+lastNameLength = lastName.length;
+
+//add .length to find character length of a string
+//eg. firstName = firstName.length
